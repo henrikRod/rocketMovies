2b8c9a4893bb42c139eb56d517c74e4fe6274797
Henrik Rodrigues Marinho <henrikrodriguesjob@gmail.com>
2024-06-11 15:05:31 -0300

    #

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>RocketMovies</h1>
+    </header>
+  );
+};
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,11 @@
 import { BrowserRouter } from "react-router-dom";
+import AppRoutes from "./app.routes";
 import AuthRoutes from "./auth.routes";
 
 export default function Routes() {
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <AppRoutes />
     </BrowserRouter>
   )
 }
